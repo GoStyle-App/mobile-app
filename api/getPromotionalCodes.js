863e2356d32f795435c4a0fafe617c16d1c15f46
amelieguers <amelie.guers@gmail.com>
2021-03-03 08:43:16 +0100

    homepage feature

---
+++ api/getPromotionalCodes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function getPromotionalCodes() {
+    const url = 'http://goreduc.herokuapp.com/api/promotional_codes';
+    return fetch(url)
+        .then((response) => response.json())
+        .then((json) => json["hydra:member"])
+        .catch((error) => console.error(error));
+}
