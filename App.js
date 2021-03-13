1e46b75356445dee743b03d3b69bfa714112c927
ilias <iliasdavid@live.fr>
2021-03-13 15:42:15 +0100

    Modification du système de navigation, ajout du composant scan : dès qu'un qrcode est scanné, il renvoie l'utilisation directement sur la page d'accueil avec en paramètre le lien récupéré, ou le contenu du qrcode. Un paramètre par défaut est envoyé à la page d'accueil, par défaut vide. L'affichage du lien sur la page de listing est temporaire, simplement pour mieux comprendre comment se servir du paramètre. Les composants sont donc liés par un menu.

--- App.js
+++ App.js
@@ -1,62 +1,114 @@
 import * as React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Image } from 'react-native';
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import { NavigationContainer } from '@react-navigation/native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+import ScanScreen from './components/scan-screen';
 import { createStackNavigator } from '@react-navigation/stack';
 import PromotionalCodes from './components/PromotionalCodes';
 import DetailPromotionalCode from './components/DetailPromotionalCode';
 
-function Logo() {
-    return (
-        <Image
-            style={{ width: 175, height: 40 }}
-            source={ require('./assets/logo.png') }
-        />
-    );
+function Nav(props) {
+  return (
+    <View style={styles.container_nav}>
+        <TouchableOpacity style={styles.button} onPress={() => props.navig.toggleDrawer()}>
+            <MaterialIcons name="menu" style={styles.menu} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.titleButton} onPress={()=>{alert("Retour à l'accueil")}}>
+          <Image
+              style={{ width: 175, height: 40 }}
+              source={require('./assets/logo.png')}
+          />
+        </TouchableOpacity>
+    </View>
+  );
 }
 
-function HomeScreen({ navigation }) {
-    return (
-        <View style={ styles.homepage }>
-            <PromotionalCodes
-                navigation={ navigation }
-            />
-        </View>
-    );
+function Home({ route, navigation }) {
+  return (
+    <View style={styles.container}>
+      <Nav navig={navigation} />
+      <PromotionalCodes route={route} navig={navigation} />
+    </View>
+  );
 }
 
-function DetailPromotionalCodeScreen({ route, navigation }) {
-    return (
-        <View>
-            <DetailPromotionalCode navigation={ navigation } route={ route }/>
-        </View>
-    )
+function Scan({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Nav navig={navigation} />
+      <ScanScreen navig={navigation}/>
+    </View>
+  )
 }
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function CustomDrawerContent(props) {
+  return (
+      <DrawerContentScrollView {...props}>
+          <DrawerItemList {...props} />
+          <DrawerItem
+              label="Close"
+              onPress={() => props.navigation.toggleDrawer()}
+          />
+      </DrawerContentScrollView>
+  );
+}
 
 function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name='Home'
-                    component={ HomeScreen }
-                    options={({ navigation }) => ({
-                        headerTitle: props => <Logo {...props} />,
-                    })}
-                />
-                <Stack.Screen
-                    name="DetailPromotionalCode"
-                    component={ DetailPromotionalCodeScreen }
-                    options={{ title: 'Détail' }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <View style={styles.container}>
+      <NavigationContainer>
+        <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
+          <Drawer.Screen name="Accueil" component={Home} initialParams={{ link: ''}} />
+          <Drawer.Screen name="Scan" component={Scan} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    homepage: {}
-})
+  container: {
+      backgroundColor: '#CAC9C8',
+      flex: 1
+  },
+  container_nav: {
+      marginTop: 30,
+      flexDirection:'row',
+      backgroundColor: 'white',
+      textAlign: "center",
+  },
+  button: {
+      backgroundColor: 'white',
+      padding: 13,
+      width: 50
+  },
+  titleButton: {
+      backgroundColor: 'white',
+      padding: 10,
+      width: 195,
+      justifyContent: "center"
+  },
+  title: {
+      fontSize: 22,
+      fontWeight: "bold",
+      justifyContent: "center"
+  },
+  menu: {
+      fontSize: 35,
+      justifyContent: 'flex-start'
+  },
+  login: {
+      fontSize: 35,
+      justifyContent: 'flex-end'
+  }
+});
 
 export default App;
