e94d39b53ceacf03586acfd2d28876d201b45261
amelieguers <amelie.guers@gmail.com>
2021-04-21 00:23:23 +0200

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,10 +29,7 @@ module.exports = {
             'error',
             4
         ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
+        'linebreak-style': 0,
         'quotes': [
             'error',
             'single'
